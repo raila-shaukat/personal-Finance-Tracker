79bdf3ff85ce866ce1915abdf478a849d3f883de
raila-shaukat <raila.shaukat@gmail.com>
2026-07-13 19:27:23 +0500

    Initialize Personal-Finance-Tracker project

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "=====================================\n";
+    cout << " Personal Finance Tracker v1.0\n";
+    cout << " Developed by Raila Shaukat\n";
+    cout << "=====================================\n";
+
+    return 0;
+}
