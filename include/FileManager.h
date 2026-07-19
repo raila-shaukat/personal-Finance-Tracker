e45e1a2da58bde3ca991da98d368215e3aa10c35
raila-shaukat <raila.shaukat@gmail.com>
2026-07-19 12:34:15 +0500

    Configure JSON storage structure

--- include/FileManager.h
+++ include/FileManager.h
@@ -4,17 +4,36 @@
 #include <vector>
 #include <string>
 
+#include "User.h"
 #include "Transaction.h"
+#include "Budget.h"
 
 class FileManager
 {
 public:
+    // User
+    static void saveUsers(const std::vector<User>& users);
+    static std::vector<User> loadUsers();
+
+    // Transactions
     static void saveTransactions(
         const std::string& username,
-        const std::vector<Transaction>& transactions);
+        const std::vector<Transaction>& transactions
+    );
 
     static std::vector<Transaction> loadTransactions(
-        const std::string& username);
+        const std::string& username
+    );
+
+    // Budgets
+    static void saveBudgets(
+        const std::string& username,
+        const std::vector<Budget>& budgets
+    );
+
+    static std::vector<Budget> loadBudgets(
+        const std::string& username
+    );
 };
 
 #endif
